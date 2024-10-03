81553499c8e96cbafbcc2a7e6c01beb48ca0750e
Урманов Артем Александрович <urmanov_aa@niipp.ru>
2024-10-03 16:53:56 +0700

    add flag cout

--- lab_1/lab1.cpp
+++ lab_1/lab1.cpp
@@ -5,19 +5,36 @@
 */
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+void print() {
+    cout << "*****" << setw(20) << "ТУСУР" << setw(20) << "*****" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "\n";
+        if(i == 5) {
+            cout.width(51);
+            cout<<" Факультет: Доп. Образование " << endl;
+            cout.width(50);
+            cout<< "Группа: 317" << endl;
+            cout.width(51);
+            cout<< "Урманов Артём\n" << endl;
+            if(i == 10) {
 
-// Вычисляет скорость прямолинейного движения (по расстоянию и времени).
-int calc_speed(int s, int t) {  
+            }
+        }
+    }
+    cout << setw(25) << "ТУСУР 2024" << setw(25) << endl;
+
+}
+
+int calc_speed(int s, int t) {
     return s / t;
 }
 
-
-// Вычисляет ускорение (по начальной скорости, конечной скорости и времени).
-int calc_boost(int v1, int v0, int t) { 
-    return (v1 - v0) / t
+int calc_boost(int v1, int v0, int t) {
+    return (v1 - v0) / t;
 }
 
 
@@ -28,13 +45,16 @@ int main()
     while((!int(a) && !int(b)) && (a < 0 && b < 0)) { // Проверка на ввод положительного числа.
         cout << "Введите значение стороны a (м):" << endl;
         cin >> a;
+        cin.ignore(32767, '\n'); // Функция игнорируюет символы кроме \n
         cout << "Введите значение стороны b (м):" << endl;
         cin >> b;
+        cin.ignore(32767, '\n');
     }
 
-    S = a * b; 
+    S = a * b; // Площадь прямоугольника.
 
     cout << "Площадь прямоугольника S = " << S << " (м)" << endl;
+    print();
 
     return 0;
 }
