2d7b8f927efb14415bc48af48abad3c1642fe280
Урманов Артем Александрович <urmanov_aa@niipp.ru>
2024-10-04 09:47:07 +0700

    final version

--- lab_1/lab1.cpp
+++ lab_1/lab1.cpp
@@ -4,12 +4,13 @@
     Номер Лабораторной работы 1.
 */
 
+
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
-void print() {
+void print_name() {
     cout << "*****" << setw(20) << "ТУСУР" << setw(20) << "*****" << endl;
     for (int i = 0; i < 10; i++) {
         cout << "\n";
@@ -26,12 +27,18 @@ void print() {
         }
     }
     cout << setw(25) << "ТУСУР 2024" << setw(25) << endl;
-
 }
+
+
+float calc_radius(int a){
+    return a / (2 * 3,14);
+}
+
 
 int calc_speed(int s, int t) {
     return s / t;
 }
+
 
 int calc_boost(int v1, int v0, int t) {
     return (v1 - v0) / t;
@@ -40,21 +47,47 @@ int calc_boost(int v1, int v0, int t) {
 
 int main()
 {
-    int a, b, S;
+    int a, b, t, S, select;
 
-    while((!int(a) && !int(b)) && (a < 0 && b < 0)) { // Проверка на ввод положительного числа.
-        cout << "Введите значение стороны a (м):" << endl;
-        cin >> a;
-        cin.ignore(32767, '\n'); // Функция игнорируюет символы кроме \n
-        cout << "Введите значение стороны b (м):" << endl;
-        cin >> b;
-        cin.ignore(32767, '\n');
+    cout << " Введите 1, если хотите скорость прямолинейного движения. \n 2 если вычислить ускорение. \n 3 вычислить радиус круга. \n 4 Вывести имя. \n" << endl;
+    cin >> select;
+
+    if(select == 4) {
+        print_name();
+        return 0;
     }
 
-    S = a * b; // Площадь прямоугольника.
+    while((!int(a) && !int(b)) && (a < 0 && b < 0)) { // Проверка на ввод положительного числа.
 
-    cout << "Площадь прямоугольника S = " << S << " (м)" << endl;
-    print();
+        cout << "Введите значение элемента:" << endl;
+        cin >> a;
+        cin.ignore(32767, '\n'); // Функция игнорируюет символы кроме
+        cout << "Введите значение второго элемента, если он необходим:" << endl;
+        cin >> b;
+        cin.ignore(32767, '\n');
+        cout << "Введите значение времени, если оно необходимо:" << endl;
+        cin >> t;
+        cin.ignore(32767, '\n');
+
+        if (select == 1) {
+            int res = calc_speed(a, b);
+
+            cout << "Cкорость прямолинейного движения v = " << res << " (м)" << endl;
+        }
+        else if (select == 2) {
+            int res = calc_boost(a, b, t);
+
+            cout << " Ускорение = " << res << endl;
+        }
+        else if (select == 3) {
+            float res = calc_radius(a);
+
+            cout << "Радиус круга R = " << res << " (cм)" << endl;
+        }
+        else {
+            cout << "Вы выбрали невеный параметр";
+        }
+    }
 
     return 0;
 }
